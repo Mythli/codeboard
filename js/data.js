d98626495b85701e581bff90732e0fb10e7a4e91
Mythli <github@mythli.net>
2014-01-07 15:55:03 +0100

    you know what i mean

--- js/data.js
+++ js/data.js
@@ -5,17 +5,15 @@ App.data = {
 	links: [
 		{
 			name: 'Stackoverflow',
-			title: 'Q&A for professional and enthusiast programmers',
 			url: 'http://stackoverflow.com/users/524041/mythli',
 			target: '_blank'
 		},
 		{
 			name: 'Github',
-			title: 'Social Coding',
 			url: 'https://github.com/mythli',
 			target: '_blank'
 		}
 	],
 
-	githubEventUrl: 'https://api.github.com/users/mythli/events',
+	githubEventUrl: 'https://api.github.com/users/mythli/events'
 };
